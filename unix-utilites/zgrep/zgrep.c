78c95ddd53ce642e555b3d2aa509eb67c3dae0bf
Ziad El-Sayed <zelsydd@gmail.com>
2025-12-01 17:31:11 +0200

    Add > and >> to zgrep

--- unix-utilites/zgrep/zgrep.c
+++ unix-utilites/zgrep/zgrep.c
@@ -1,11 +1,14 @@
 
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 void search_and_print(FILE *fp, char *str, int strSize) {
+
   char *buff = NULL;
   size_t size = 0;
 
@@ -38,6 +41,39 @@ int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("wgrep: searchterm [file ...]\n");
     exit(1);
+  }
+
+  int out = -1;
+  bool append = 0;
+  // detect > or >>
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], ">") == 0 || strcmp(argv[i], ">>") == 0) {
+      // no assign file
+      if (i + 1 >= argc) {
+        printf("wzip: missing output file\n");
+        exit(1);
+      }
+      append = strcmp(argv[i], ">>") == 0;
+
+      if (append) {
+        out = open(argv[i + 1], O_WRONLY | O_CREAT | O_APPEND,
+                   0644); // 0644 -rw-r--r--
+      } else
+        out = open(argv[i + 1], O_WRONLY | O_CREAT | O_TRUNC,
+                   0644); // 0644 -rw-r--r--
+
+      if (out < 0) {
+        printf("error at openning output file\n");
+        exit(1);
+      }
+
+      // replace stdout file descriptor "1" with out value
+      dup2(out, STDOUT_FILENO); // sdtout file # is 1
+      close(out);
+
+      argc = i; // only files before > or >> are input files
+      break;
+    }
   }
 
   char str[100];
